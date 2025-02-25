ea985a912d91a9a9e1fcb2ee9495364f15dee749
gobbios <christofneumann1@gmail.com>
2025-02-25 12:01:00 +0100

    add latex check and restructure output

--- R/settings_check.R
+++ R/settings_check.R
@@ -1,5 +1,7 @@
 #' some checks for how a system is set up with respect to R, RStudio and git
 #'
+#' @param r_packages a character vector with package names. default is
+#'          \code{NULL}, i.e. don't check any package availability.
 #' @returns textual output
 #' @export
 #'
@@ -11,17 +13,14 @@
 #'
 #' @examples
 #' settings_check()
-settings_check <- function() {
+#' settings_check(r_packages = c("lme4", "EloSteepness"))
+settings_check <- function(r_packages = NULL) {
   if (Sys.info()["sysname"] == "Darwin") os <- "mac"
   if (Sys.info()["sysname"] == "Windows") os <- "win"
   if (Sys.info()["sysname"] == "Linux") os <- "linux"
 
   all_good <- TRUE
 
-  if (!"lme4" %in% installed.packages()[, "Package"]) {
-    cat("package lme4 is missing\n")
-    all_good <- FALSE
-  }
 
   # check if you have git installed:
   gitpresent <- tryCatch({
@@ -31,6 +30,9 @@ settings_check <- function() {
   }, warning = function(e) {
     return(NULL)
   })
+
+  cat("checking git installation:\n")
+  cat("--------------------------\n")
   if (is.null(gitpresent)) {
     cat("it seems git is not installed\n\n")
   } else {
@@ -46,15 +48,35 @@ settings_check <- function() {
   }, warning = function(e) {
     return(NULL)
   })
+
+  cat("checking quarto installation:\n")
+  cat("-----------------------------\n")
   if (is.null(quartopresent)) {
     cat("it seems quarto is not installed\n\n")
   } else {
     cat("wohoo, it seems you have quarto (version ", quartopresent, " to be precise)\n\n", sep = "")
   }
 
+  # check if LaTex installed:
+  latexpressent <- tryCatch({
+    system2("latex", "-v", stdout = TRUE, stderr = NULL)
+  }, error = function(e) {
+    return(NULL)
+  }, warning = function(e) {
+    return(NULL)
+  })
+  cat("checking LaTeX installation:\n")
+  cat("----------------------------\n")
+  if (is.null(quartopresent)) {
+    cat("it seems LaTeX is not installed\n\n")
+  } else {
+    cat("wohoo, it seems you have LaTeX (I don't dare to guess which version exactly)\n\n", sep = "")
+  }
 
 
   # Rstudio if active
+  cat("checking RStudio installation:\n")
+  cat("------------------------------\n")
   if ("rstudioapi" %in% installed.packages()[, "Package"]) {
     if (os == "mac" | os == "linux") {
       rstudioprefs <- suppressWarnings(readLines("~/.config/rstudio/rstudio-prefs.json"))
@@ -99,9 +121,22 @@ settings_check <- function() {
     cat(" (or at least it's not been used with the current R installation)\n")
   }
 
-  if (all_good) {
-    cat("looking good\n")
+
+  if (!is.null(r_packages)) {
+    cat("\n")
+    cat("checking R packages installation:\n")
+    cat("---------------------------------\n")
+
+    for (pk in r_packages) {
+      if (!pk %in% installed.packages()[, "Package"]) {
+        cat("NOOOO, package", shQuote(pk), "is missing\n")
+      } else
+        cat("nice, package", shQuote(pk), "is available\n")
+    }
   }
+
+
+
   invisible(NULL)
 }
 
