fcb11e8c601bb22d477726c06f1ad585a6e07365
gobbios <christofneumann1@gmail.com>
2025-02-28 11:14:41 +0100

    partner sex check added

--- cleaning_script.R
+++ cleaning_script.R
@@ -1,18 +1,36 @@
-xdata <- read.csv("data/reconcilation_data_v2.csv")
+xdata <- read.csv("data/reconcilation_data_v2.csv", header = TRUE, dec = ".")
 
 if (!is.data.frame(xdata)) stop("input is not a data frame")
 
+if (nrow(xdata) != 914) stop("not enough lines in the data")
+
 if (any(duplicated(xdata$CaseNr))) stop("duplicated CaseNr")
 
+
+str(xdata)
 xdata$PartSex <- tolower(substr(xdata$PartSex, 1, 1))
+
+if (length(na.omit(unique(xdata$PartSex))) != 2) stop("more than two sexes")
+
+xdata$RconDur <- as.numeric(gsub(pattern = ",", replacement = ".", x = xdata$RconDur, fixed = TRUE))
+
+
 temp <- table(xdata$Partner, xdata$PartSex, useNA = "always") > 0
 check <- rownames(temp[rowSums(temp) > 1 ,])
 if (length(check) > 0) {
   stop("Partner ids in 'check' have sex issues")
 }
 
-check <- which(xdata$Subject == xdata$Partner)
-if (length(check) > 0) {
-  stop("rows in check: partner is the same as the subject")
-}
+
+head(table(xdata$Partner, xdata$PartSex))
+# 'And' as male occurs once, but it could be 'Ant'
+
+
+
+
+
+
+
+
+
 
